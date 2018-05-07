7a3100ce382d54984b2c656fed4719ee01a3c884
Muhammad Hafiz Bin Misron <hafiz@oc2275431667.ibm.com>
2018-05-07 11:14:33 +0800

    streamlined code with processText function

--- app.js
+++ app.js
@@ -7,27 +7,18 @@ const assistant = new AssistantV1({
     password: process.env.CONVPWORD
 });
 
-// ----- For TELEGRAM -----
+// ---------- For TELEGRAM ----------
 const TelegramBot = require('node-telegram-bot-api');
 const tbot = new TelegramBot(process.env.TELEGRAM, {polling: true});
 
-tbot.on('message', msg => {
-    assistant.message({
-        workspace_id: process.env.WORKSPACEID,
-        input: {'text': msg.text}
-    }, (watsonErr, watsonResponse) => {
-        if(watsonErr) {
-            console.log(watsonErr);
-        }
-        else {
-            tbot.sendMessage(msg.chat.id, watsonResponse.output.text[0]);
-        }
-    });
+tbot.on('message', async function(msg) {
+    let response = await processText(msg.text);
+    tbot.sendMessage(msg.chat.id, response);
 });
-// ----- For TELEGRAM -----
+// ---------- For TELEGRAM ----------
 
 
-// ----- For SLACK ----- 
+// ---------- For SLACK ----------
 const Botkit = require('botkit');
 const controller = Botkit.slackbot({debug: false});
 const sbot = controller.spawn({token: process.env.APITOKEN});
@@ -37,18 +28,24 @@ sbot.startRTM();
 controller.hears(
     ['.*'], 
     ['direct_message', 'direct_mention', 'mention'],
-    function(bot, msg) {
-        assistant.message({
-            workspace_id: process.env.WORKSPACEID,
-            input: {'text': msg.text}
-        }, (watsonErr, watsonResponse) => {
-            if(watsonErr) {
-                console.log(watsonErr);
-            }
-            else {
-                bot.reply(msg, watsonResponse.output.text[0]);
-            }
-        });
+    async function(bot, msg) {
+        let response = await processText(msg.text);
+        bot.reply(msg, response);
     }
 );
-// ----- For SLACK -----
+// ---------- For SLACK ----------
+
+
+function processText(message) {
+    return new Promise(resolve => {
+        assistant.message({
+            workspace_id: process.env.WORKSPACEID,
+            input: {'text': message}
+        }, (err, resp) => {
+            if(err) {
+                console.log(err);
+            }
+            resolve(resp.output.text[0]);
+        });
+    });
+}
